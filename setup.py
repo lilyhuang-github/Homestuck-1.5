0601b858c4ed9abe6a5031008e24c05cd5f3d104
Lily <112970249+lilyhuang-github@users.noreply.github.com>
2024-12-19 19:50:40 -0500

    updated dockerignore ifle

--- setup.py
+++ setup.py
@@ -262,12 +262,12 @@ class character:
         return dialogue
     
 
-firstTimeSetup()
+# firstTimeSetup()
 # def 
 # firstTimeSetup()
-# t = loadDialogue("./ngram/TP.json")
-# c4 = character(t, "TP")
-# print(c4.getRandomDialogue())
+t = loadDialogue("./ngram/TP.json")
+c4 = character(t, "TP")
+print(c4.getRandomDialogue())
 # d = loadDialogue("./ngram/VS.json")
 # getSimpleDialogue(d)
 # d.getRandomDialogue()
